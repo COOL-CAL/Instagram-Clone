9dc98f883bd581ba41f84f82ab6518de22449fcc
COOL_CAL <grin0330@gmail.com>
2022-07-01 18:06:21 +0900

    오후수업

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -28,12 +28,30 @@ class UserModel extends Model {
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
 
-    public function selUserByIuser(&$param) {
-        $sql = "SELECT iuser, email, nm, cmt, mainimg, regdt 
+    // public function selUserByIuser(&$param) {
+    //     $sql = "SELECT iuser, email, nm, cmt, mainimg, regdt 
+    //               FROM t_user
+    //              WHERE iuser = :iuser";
+    //     $stmt = $this->pdo->prepare($sql);
+    //     $stmt->bindValue(":iuser", $param["iuser"]);
+    //     $stmt->execute();
+    //     return $stmt->fetch(PDO::FETCH_OBJ);
+    // }
+
+    public function selUserProfile(&$param) {
+        // $sql = "SELECT A.iuser, A.email, A.nm, A.cmt, A.mainimg COUNT(B.ifeed)
+        //         FROM t_user A
+        //         INNER JOIN t_feed B
+        //         WHERE A.iuser = B.iuser AND B.iuser = :iuser";
+        $feediuser = $param["feediuser"];
+        $loginiuser =  $param["loginiuser"];
+        $sql = "SELECT iuser, email, nm, cmt, mainimg
+                     , (SELECT COUNT(ifeed) FROM t_feed WHERE iuser = {$feediuser}) AS feedCnt
+                     , (SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser = {$feediuser} AND toiuser = {$loginiuser}) AS youme
+	                 , (SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser = {$loginiuser} AND toiuser = {$feediuser}) AS meyou
                   FROM t_user
-                 WHERE iuser = :iuser";
+                 WHERE iuser = {$feediuser}";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(":iuser", $param["iuser"]);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
